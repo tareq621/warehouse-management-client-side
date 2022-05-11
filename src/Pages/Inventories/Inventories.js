0f9bb0590f0634c2ae9348b5f3fc444adb8225d8
tareq621 <mirtareq621@gmail.com>
2022-05-12 00:11:29 +0600

    inventory id

--- src/Pages/Inventories/Inventories.js
+++ src/Pages/Inventories/Inventories.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import Inventory from './Inventory/Inventory';
 
@@ -15,13 +16,50 @@ const Inventories = () => {
 
     return (
         <div>
-            <div className='inventory-container'>
-                {
-                    inventories.map(inventory => <Inventory
-                        key={inventory._id}
-                        inventory={inventory}
-                    ></Inventory>)
-                }
+            <div className="card mb-3 border-0" >
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img className='img-fluid rounded-start mt-4 ms-5' src={inventories.img} alt="" />
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <div className="card-body ms-5">
+                            <table className="table caption-top">
+                                <tbody>
+                                    <tr>
+                                        <td className='fw-bold'>Id:</td>
+                                        <td>{inventories._id}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='fw-bold'>Name:</td>
+                                        <td>{inventories.name}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='fw-bold'>Price:</td>
+                                        <td>${inventories.price}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='fw-bold'>Quantity:</td>
+                                        <td>{inventories.quantity}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='fw-bold'>Supplier</td>
+                                        <td>{inventories.supplierName}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className='fw-bold'>Description:</td>
+                                        <td>{inventories.description}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div className='d-flex'>
+                                <Form.Group className="w-50">
+                                    <Form.Control className='border-0 border-bottom border-bottom' name='name' type="number" placeholder="stock" />
+                                </Form.Group>
+                                <Button style={{ backgroundColor: '#E21717' }} className='border-0 text-light px-3 ms-4' type="submit">Re-stock</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
